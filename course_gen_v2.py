039e844e94521235389d00e4f403cfb1904728fd
jakesurry <jacobsurry05@gmail.com>
2026-01-31 16:04:35 -0800

    tweak to fairway generator, more uniform. Add roughs

--- course_gen_v2.py
+++ course_gen_v2.py
@@ -1,9 +1,9 @@
 import numpy as np
-np.random.seed(5)
-
 from scipy.interpolate import BSpline, PchipInterpolator, splprep, splev
 import matplotlib.pyplot as plt
 from matplotlib.path import Path
+
+np.random.seed(10)
 
 class Course():
     def __init__(self, par=4, dogleg=False, intensity=1):
@@ -18,7 +18,9 @@ class Course():
         self.green = [[]]
         self.green_path = None
 
+
     def _generate_length(self):
+        length = 0
         match self.par:
             case 3:
                 length = 150
@@ -28,6 +30,7 @@ class Course():
                 length = 550
 
         return length
+    
 
     def _generate_center_line(self, length):
         inflection_points = 7
@@ -37,7 +40,7 @@ class Course():
         if self.dogleg == True:
             bend_location = 3 # 3 is central
             bend_direction = 1 # 1 arcs up, -1 arcs down
-            intensity_scaler = 30 # arbitrary
+            intensity_scaler = 50 # arbitrary
             bend_intensity = self.intensity * intensity_scaler * bend_direction
             for i in range(1, inflection_points-1):
                 y_points[i] = bend_intensity * np.exp(-((i-bend_location)/2)**2)
@@ -45,43 +48,62 @@ class Course():
         center_line_function = PchipInterpolator(x_points, y_points)
         
         return center_line_function
+    
 
     def _generate_width(self, length):
-        degree = 6
+        degree = 5
         num_control_points = 10
         control_array = np.zeros(num_control_points + degree+1)
         knot_array = np.linspace(0, length, num_control_points+2)
-        knot_array = np.concatenate((np.zeros(degree),  \
-                                     knot_array,        \
+        knot_array = np.concatenate((np.zeros(degree),
+                                     knot_array,
                                      np.full(shape=degree, fill_value=knot_array[-1]))) #Duplicate boundry knots
 
-        min_width = 20
+
+        min_width = 10
         for i in range(len(control_array)):
-            offset = abs(min_width//2 + (min_width//2)*np.random.uniform(low=0.0, high=2.0))
+            loop_pos = (i/(len(control_array)-1)) * 2 # 0 -> 2, 0 at i = 0, 2 at i = n (last loop)
+            center_scaler = -2 * loop_pos * (loop_pos - 2) # 0 -> 2, 0 at i = 0, 2 at i = n// 2, 0 at i = n (quadratic)
+            center_scaler *= min_width // 2
+            # offset = min_width//2 + (min_width)*np.random.uniform(low=0.0, high=2.0)*center_scaler
+            # offset = abs(min_width//2 + (min_width // 2)*np.random.randn() + center_scaler)
+            offset = min_width + abs((min_width)*np.random.randn())
             control_array[i] = offset
 
         control_array[0] = 0
-        control_array[-1] = 0
+        control_array[-1] = min_width
+        control_array[-4] = min_width // 4
 
         width_function = BSpline(knot_array, control_array, degree)
 
         return width_function
+    
     
     def _generate_fairway_shape(self, length, center_line, top_width, bottom_width):
         xx = np.linspace(0, length)
 
         top_bound = center_line(xx) + top_width(xx)
         bottom_bound = center_line(xx) - bottom_width(xx)
+        fairway_x = np.concatenate((xx, xx[-1:0:-1]))
+        fairway_y = np.concatenate((top_bound, bottom_bound[-1:0:-1]))
 
-        fairway_x = np.concatenate((xx, xx[-2:0:-1]))
-        fairway_y = np.concatenate((top_bound, bottom_bound[-2:0:-1]))
-
-        shape_generation_resolution = np.linspace(0, 1, length)
-        tck, u = splprep([fairway_x, fairway_y], s=0, per=True)
-        fairway = splev(shape_generation_resolution, tck)
+        fairway_generation_resolution = np.linspace(0, 1, length)
+        fairway_tck, _ = splprep([fairway_x, fairway_y], s=0, per=True)
+        fairway = splev(fairway_generation_resolution, fairway_tck)
         fairway_path = Path(np.column_stack([fairway[0], fairway[1]]))
 
-        return fairway, fairway_path
+        rough_top_bound = top_bound + 20
+        rough_bottom_bound = bottom_bound - 20
+        rough_x = np.concatenate((xx, xx[-1:0:-1]))
+        rough_y = np.concatenate((rough_top_bound, rough_bottom_bound[-1:0:-1]))
+
+        rough_generation_resolution = np.linspace(0, 1, length//4)
+        rough_tck, _ = splprep([rough_x, rough_y], s=0, per=True)
+        rough = splev(rough_generation_resolution, rough_tck)
+        rough_path = Path(np.column_stack([rough[0], rough[1]]))
+
+        return fairway, fairway_path, rough, rough_path
+    
     
     def _generate_green_shape(self, length, fairway):
         green_length = 30
@@ -105,41 +127,53 @@ class Course():
         green_path = Path(np.column_stack((green[0], green[1])))
         
         return green, green_path
-    
+
+
     def generate(self):
         length = self._generate_length()
         center_line = self._generate_center_line(length)
         top_width = self._generate_width(length)
         bottom_width = self._generate_width(length)
-        fairway, fairway_path = self._generate_fairway_shape(length, center_line, top_width, bottom_width)
+        fairway, fairway_path, rough, rough_path = self._generate_fairway_shape(length, center_line, top_width, bottom_width)
         green, green_path = self._generate_green_shape(length, fairway)
 
         self.center_line = center_line
         self.fairway = fairway
         self.fairway_path = fairway_path
+        self.rough = rough
+        self.rough_path = rough_path
         self.green = green
         self.green_path = green_path
+
 
     def on_fairway(self):
         return bool
 
-course = Course(dogleg=True)
-course.generate()
 
-center_line = course.center_line
-fairway = course.fairway
-green = course.green
-xx = np.linspace(0, course.length, 50)
+def main():
+    course = Course(dogleg=True)
+    course.generate()
 
-fig, ax = plt.subplots()
+    center_line = course.center_line
+    fairway = course.fairway
+    rough = course.rough
+    green = course.green
+    xx = np.linspace(0, course.length, 50)
 
-ax.plot(xx, center_line(xx), 'r-', label='Center Line')
-ax.plot(fairway[0], fairway[1], 'b-', linewidth=2, label='Fairway')
-ax.fill(fairway[0], fairway[1], alpha=0.3, color='forestgreen')
-ax.plot(green[0], green[1], 'g-', linewidth=2, label='Green')
-ax.fill(green[0], green[1], alpha=0.3, color='limegreen')
+    fig, ax = plt.subplots()
 
-ax.grid(True)
-ax.legend(loc='best')
-ax.axis('equal')
-plt.show()
+    # ax.plot(xx, center_line(xx), 'r-', label='Center Line')
+    # ax.plot(rough[0], rough[1], 'b-', linewidth=2, label='Rough')
+    ax.fill(rough[0], rough[1], alpha=.5, color='seagreen')
+    # ax.plot(fairway[0], fairway[1], 'b-', linewidth=2, label='Fairway')
+    ax.fill(fairway[0], fairway[1], alpha=0.7, color='forestgreen')
+    # ax.plot(green[0], green[1], 'g-', linewidth=2, label='Green')
+    ax.fill(green[0], green[1], alpha=0.7, color='limegreen')
+
+    ax.grid(True)
+    ax.legend(loc='best')
+    ax.axis('equal')
+    plt.show()
+
+if __name__ == "__main__":
+    main()
